b8446d25fbb1f00a5d0cd8d43d65b21e91417d77
Tom Radford <tom@theradford.com>
2024-02-27 17:36:02 +0200

    ex 10.3

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { Pressable, Text } from 'react-native';
-
-export const Button = ({ text, onPress }) => (
-  <Pressable onPress={onPress}>
-    <Text>{text}</Text>
-  </Pressable>
-);
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,23 @@
 import Constants from 'expo-constants';
 import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './RepositoryList';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flexGrow: 1,
-    flexShrink: 1,
-    color: '#234'
+    flexShrink: 1
+  },
+  header: {
+    marginBottom: 4
   }
 });
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <Text>Rate Repository App</Text>
+      <Text style={styles.header}>Rate Repository App</Text>
+      <RepositoryList />
     </View>
   );
 };
