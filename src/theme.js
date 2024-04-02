90864c661463d0c95fa26ef28ea05485e1059c0e
Tom Radford <tom@theradford.com>
2024-04-02 10:26:05 +0200

    ex 10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -16,7 +18,7 @@ const theme = {
     subheading: 16
   },
   fonts: {
-    main: 'System'
+    main: Platform.select({ default: 'System', android: 'Roboto', ios: 'Arial' })
   },
   fontWeights: {
     normal: '400',
