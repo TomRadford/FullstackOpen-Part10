242cb2b892f186298318126a80941d4ed6b64264
Tom Radford <tom@theradford.com>
2024-04-02 10:03:07 +0200

    use BrowserRouter for web target

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
+import { BrowserRouter } from 'react-router-dom';
+import { Platform } from 'react-native';
+
+// Allows us to use the Browser-friendly router provider
+const Router = Platform.select({
+  ios: () => NativeRouter,
+  android: () => NativeRouter,
+  web: () => BrowserRouter
+})();
 
 export default function App() {
   return (
     <>
-      <NativeRouter>
+      <Router>
         <Main />
-      </NativeRouter>
+      </Router>
       <StatusBar style="inverted" />
     </>
   );
