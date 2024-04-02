a29148033fef544a59cb373f2e9c57c1f5d2700f
Tom Radford <tom@theradford.com>
2024-04-02 10:16:53 +0200

    import the metro runtime for HMR on web

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
 import { BrowserRouter } from 'react-router-dom';
 import { Platform } from 'react-native';
+import '@expo/metro-runtime';
 
 // Allows us to use the Browser-friendly router provider
 const Router = Platform.select({
