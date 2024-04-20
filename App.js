ef40c991828d150d63aa31313709b0bc5ea65a07
Tom Radford <tom@theradford.com>
2024-04-20 11:49:34 +0200

    add apollo and metro config (for cjs support)

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NativeRouter } from 'react-router-native';
 import { BrowserRouter } from 'react-router-dom';
 import { Platform } from 'react-native';
 import '@expo/metro-runtime';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
 
 // Allows us to use the Browser-friendly router provider
 const Router = Platform.select({
@@ -12,11 +14,15 @@ const Router = Platform.select({
   web: () => BrowserRouter
 })();
 
+const apolloClient = createApolloClient();
+
 export default function App() {
   return (
     <>
       <Router>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </Router>
       <StatusBar style="inverted" />
     </>
